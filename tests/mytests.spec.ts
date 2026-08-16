e3078c0a6940222fae2c6856e921884c9ea92e7b
Aravind Juloori <julooriaravind@gmail.com>
2026-08-16 19:19:22 +0530

    first playwright test

---
+++ tests/mytests.spec.ts
@@ -0,0 +1,13 @@
+import{test,expect} from "@playwright/test"
+
+test("Verfify the page title", async ({page})=>{
+
+    await page.goto("https://www.snapfish.com");
+    let title:String=await page.title();
+    console.log("Title of the page is :",title);
+   await expect(page).toHaveTitle("Snapfish | Personalized Gifts, Cards, Home Decor, Photo Books & More ");
+    
+
+
+
+})
