c68c224b586786382cd675d4135521ae7eefad22
Aravind Juloori <julooriaravind@gmail.com>
2026-09-07 22:50:27 +0530

    Pushing playwright locators concepts

--- tests/PwLocators.spec.ts
+++ tests/PwLocators.spec.ts
@@ -1,88 +1,91 @@
 /*
-page.getByAltText() to locate an element, usually image, by its text alternative.
-page.getByText() to locate by text content.
 page.getByRole() to locate by explicit and implicit accessibility attributes.
+page.getByText() to locate by text content.
 
 page.getByLabel() to locate a form control by associated label's text.
 page.getByPlaceholder() to locate an input by placeholder.
+
+page.getByAltText() to locate an element, usually image, by its text alternative.
 
 page.getByTitle() to locate an element by its title attribute.
 page.getByTestId() to locate an element based on its data-testid attribute (other attributes can be configured).
 */
 
-import{test,expect,Locator} from "@playwright/test"
+import{test,expect,Locator} from "@playwright/test";
 
+//page.getByRole()
+test("Playwright Locators getByRole",async ({page})=>{
 
-//page.getByAltText() to locate an element, usually image, by its text alternative.
-test("Verify Playwright getByAltText",async({page})=>{
+    await page.goto("https://sdetqa.vercel.app/pw-locators-demo-app");
+    const projectlink:Locator=page.getByRole("link",{name:"Projects"});
+    await expect(projectlink).toBeVisible();
 
-    await page.goto("https://rahulshettyacademy.com/");
-    const logo:Locator=page.getByAltText("Rahul Shetty Academy");
-    await expect(logo).toBeVisible();
-})
+    const signInbutton:Locator=page.getByRole("button",{name:"Sign In"});
+    await expect(signInbutton).toBeVisible();
 
-//page.getByText() to locate by text content.Its just like non-interactive elements like div,span,p,h2
-
-test.only("Verify playwright getByText",async({page})=>{
-
-   await page.goto("https://rahulshettyacademy.com/locatorspractice/");
-
-   const remeberText:Locator=page.getByText("Remember my username");
-   await expect(remeberText).toBeVisible();
-    await expect(page.getByText("Remember my ")).toBeVisible();  //substring/partial text
-     await expect(page.getByText(/remember\s+My\s+Username/i)).toBeVisible();  //Regular Expression 
-})
-
-//page.getByRole() to locate by explicit and implicit accessibility attributes.(interactive)
-
-test("Verify by getByRole",async({page})=>{
-
-    await page.goto("https://rahulshettyacademy.com/locatorspractice/");
-    await page.getByRole("link",{name:'Forgot your password?'}).click();
-    await expect(page.getByRole("heading",{name:'Forgot password'})).toBeVisible();
-
-    await page.waitForTimeout(3000);
-})
-
-//4.page.getByLable()-Locate by using the lable text
-
-test("Verify by using getByLabel",async({page})=>{
-
-    await page.goto("http://127.0.0.1:5500/tests/app.html");
-    await page.getByLabel("Email Address:").fill("aravind.juloori@gmail.com");
-     const emailTextBox:Locator=page.locator("#email");
-     const enteredvalue:string=await emailTextBox.inputValue();
-     expect(enteredvalue).toBe("aravind.juloori@gmail.com")
-
-
-    await page.waitForTimeout(5000);
-})
-
-//5.page.getByPlaceHolder()
-
-test("Verify by using the getByPlaceHolder",async({page})=>{
-    await page.goto("https://testautomationpractice.blogspot.com/");
-    await page.getByPlaceholder("Enter Name").fill("Aravind");
+    await signInbutton.click();
 
 })
 
-//6.page.getByTitle()
-test("Verify by using getByTitle",async({page})=>{
 
-   await page.goto("http://127.0.0.1:5500/tests/app.html") 
-   const title:Locator=page.getByTitle("Home page link");
-//    await title.click();
-   await expect(title).toHaveText("Home");
-
-   await expect(page.getByTitle("HyperText Markup Language")).toHaveText("HTML")
+//getByText 
+test("Playwright Locators getByText",async ({page})=>{
+     await page.goto("https://sdetqa.vercel.app/pw-locators-demo-app");
+    const welcomeText=page.getByText("Welcome, John! 👋 ",{exact:true});
+    await expect(welcomeText).toBeVisible();
 })
 
-//6.page.getBydata-testId()
-test("Verify by using getBydata-testId",async({page})=>{
-
-        await page.goto("http://127.0.0.1:5500/tests/app.html") 
-        const email:Locator=page.getByTestId("profile-email");
-        await expect(email).toHaveText("john.doe@example.com");
-        await expect(page.getByTestId("profile-name")).toHaveText("John Doe");
-   
+//page.getByLabel()
+test("Playwright Locators getByLabel",async ({page})=>{
+     await page.goto("https://sdetqa.vercel.app/pw-locators-demo-app");
+     const emailField= page.getByLabel("Email Address");
+     await expect(emailField).toBeVisible();
+     await emailField.fill("Aravind.juloori@gmail.com");
 })
+
+//page.getByPlaceholder()
+test("Playwright Locators getByPlaceholder",async ({page})=>{
+     await page.goto("https://sdetqa.vercel.app/pw-locators-demo-app");
+     const searchInput=page.getByPlaceholder("Search tests...");
+     await expect(searchInput).toBeVisible();
+     await searchInput.fill("Locators practise");
+})
+
+
+      // 5) getByAltText() - Locate an image by its alt attribute text
+  //identifies images (and similar elements) based on the alt attribute.
+  // Use this locator when your element supports alt text such as img and area elements.
+test("Playwright Locators getByAltText",async ({page})=>{
+     await page.goto("https://sdetqa.vercel.app/pw-locators-demo-app");
+    await expect(page.getByAltText("Playwright logo")).toBeVisible();
+})
+
+
+  // 6) getByTitle() - Locate an element using its title attribute
+  // When to use: When your element has a meaningful title attribute.
+  test("Playwright Locators getByTitle",async ({page})=>{
+     await page.goto("https://sdetqa.vercel.app/pw-locators-demo-app");
+    
+     const totalrunsbox=page.getByTitle("Total test runs");
+    await expect(totalrunsbox).toBeVisible();
+    await expect(totalrunsbox).toHaveText('4,821Total Runs');
+    await expect(totalrunsbox).toContainText('4,821');
+    
+})
+
+ // 7) getByTestId() - Locate elements using the data-testid attribute
+  //Locate an element based on its data-testid attribute (other attributes can be configured)
+  // When to use: When text or role-based locators are unstable or not suitable.
+
+test.only("Playwright Locators getByTestId",async ({page})=>{
+     await page.goto("http://127.0.0.1:5500/apps/pw-locators-demo-app.html");
+    
+     const proPlanButton= page.getByTestId('add-to-cart-pro');
+    await expect(proPlanButton).toBeVisible();
+    await proPlanButton.click();
+    
+    const searchButton=page.getByTestId('search-button');
+    
+})
+
+//https://sdetqa.vercel.app/pw-locators-practice-app
